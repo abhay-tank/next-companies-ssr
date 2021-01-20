2f4f6b2634618e5836e1a2ffd9784f56982190b3
abhay-tank <abhay.tank@raweng.com>
2021-01-20 20:48:15 +0530

    Initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title key="title">Surfboard Ventures</title>
+			</Head>
+			<h1>Surfboard</h1>
+		</div>
+	);
+}
