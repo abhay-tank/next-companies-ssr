c281c450d74c504d3a458c1bcb83872d64024eb4
abhay-tank <abhay.tank@raweng.com>
2021-01-21 01:04:45 +0530

    Created navbar and surfboard index page

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import fetchData from "../../helper/fetchData";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../../styles/Navbar.module.css";
+function Navbar(props) {
+	let [links, setLinks] = useState([]);
+	let [error, setError] = useState(null);
+	useEffect(async () => {
+		if (!links.length) {
+			try {
+				const url = "https://abhay-tank.github.io/json-server/navbarData.json";
+				let data = await fetchData(url);
+				setLinks(data);
+			} catch (err) {
+				console.error(err);
+				if (!error) {
+					setError(error);
+				}
+			}
+		}
+	}, []);
+	if (error) {
+		return <h1>Error fetching data</h1>;
+	} else {
+		return (
+			<div className={styles["navbarContainer"]}>
+				{links.map((link) => {
+					return (
+						<Link key={link.id} href={link.URL}>
+							<div className={styles["navLink"]}>
+								<Image
+									className={styles["linkImage"]}
+									height="60"
+									width="60"
+									src={link.logoURL}
+									alt={link.title}
+								/>
+							</div>
+						</Link>
+					);
+				})}
+			</div>
+		);
+	}
+}
+
+export default Navbar;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,73 @@
 import Head from "next/head";
-export default function Home() {
-	return (
-		<div>
-			<Head>
-				<title key="title">Surfboard Ventures</title>
-			</Head>
-			<h1>Surfboard</h1>
-		</div>
-	);
+import Image from "next/image";
+import fetchData from "../helper/fetchData";
+import Navbar from "../components/Navbar/Navbar";
+export default function Home(props) {
+	if (props.hasError) {
+		return <h1>Error loading page</h1>;
+	} else {
+		let {
+			id,
+			title,
+			logoURL,
+			companyName,
+			companyTagline,
+			backgroundColor,
+			sections,
+		} = props.data;
+		return (
+			<div>
+				<Head>
+					<title key="title">{title}</title>
+					<link rel="shortcut icon" type="image/x-icon" href={`/${id}.ico`} />
+				</Head>
+				<Navbar />
+				<div className="container" style={{ backgroundColor: backgroundColor }}>
+					<Image src={logoURL} height="300" width="300" alt="logoImage" />
+					<h1>{companyName}</h1>
+					<h3>{companyTagline}</h3>
+				</div>
+				{sections.map((section) => {
+					return (
+						<div
+							style={{
+								backgroundColor: section.backgroundColor,
+								color: section.fontColor,
+							}}
+							key={section.sectionId}
+							className="container section"
+						>
+							<div className="col">
+								<Image
+									src={section.bannerImageURL}
+									height="500"
+									width="500"
+									alt="bannerImage"
+								/>
+							</div>
+							<div className="col">
+								<h1>{section.title}</h1>
+								<h3>{section.subtitle}</h3>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
 }
+
+export const getStaticProps = async (context) => {
+	const url = "https://abhay-tank.github.io/json-server/surfboardData.json";
+	const data = await fetchData(url);
+	if (data instanceof Error) {
+		return {
+			props: {
+				hasError: true,
+			},
+		};
+	}
+	return {
+		props: { data }, // will be passed to the page component as props
+	};
+};
